04478690fe4ea830f79c4a4ba3aed29b6ace8577
Chinmay-Ghaskadbi <69080588+Chinmay-Ghaskadbi@users.noreply.github.com>
2022-11-18 15:29:09 -0500

    Add files via upload

---
+++ STAT-184 EDA Draft.R
@@ -0,0 +1,7 @@
+## What decade has the most popular games and what genre is the game?
+
+library(readr)
+
+dataforEDA <- read.csv(file = "steamdata", header=TRUE, stringsAsFactors=FALSE)
+head(dataforEDA)
+
